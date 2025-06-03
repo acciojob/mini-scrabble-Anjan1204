670554bed098e999580690ba24aa207fdc44f9f8
Anjani Kumar <anjan.1204kr@gmail.com>
2025-06-03 13:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+const inputBox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+inputBox.addEventListener("input", () => {
+  // Count all characters including spaces and special characters
+  letterCount.textContent = inputBox.value.length;
+});
